da9cd7687fef2e0d04ef67ebfc808b44b76eb3b2
Adarsh Reddy <adarshreddy65@gmail.com>
2020-06-15 15:27:58 +0530

    Updates

---
+++ precache-manifest.d895c43f7e3de834723a7a38c6fbfa67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97bf35555e58ce5d9f5472352c94648d",
+    "url": "/Project-COVID19/index.html"
+  },
+  {
+    "revision": "2ad3094b378cb6ee5c18",
+    "url": "/Project-COVID19/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "11cc73d9548ddd73655a",
+    "url": "/Project-COVID19/static/css/main.b573a58c.chunk.css"
+  },
+  {
+    "revision": "2ad3094b378cb6ee5c18",
+    "url": "/Project-COVID19/static/js/2.956910c4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Project-COVID19/static/js/2.956910c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11cc73d9548ddd73655a",
+    "url": "/Project-COVID19/static/js/main.d7814904.chunk.js"
+  },
+  {
+    "revision": "df528518eec098d88482",
+    "url": "/Project-COVID19/static/js/runtime-main.1286ce3d.js"
+  },
+  {
+    "revision": "739172635e31666997fc7bd7851fe974",
+    "url": "/Project-COVID19/static/media/corona.73917263.jpg"
+  }
+]);
